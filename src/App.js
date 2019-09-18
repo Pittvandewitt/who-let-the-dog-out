6cb27de8543a3f75efaff51cd96d85badfea615c
Rezan92 <rezanibrahim8@gmail.com>
2019-09-18 13:27:04 +0200

    Using Route to navigate between components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import React from 'react';
 import './App.css';
 import DogsListContainer from './components/DogsListContainer';
 import DogBreedsImagesContainer from './components/DogBreedsImagesContainer';
-import { Route } from 'react-router-dom'
+import Home from './components/Home'
+import Game1 from './components/Game1'
+import Game2 from './components/Game2'
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Route exact path="/" component={DogsListContainer} />
-        <Route path="/dog-breeds/:breed" component={DogBreedsImagesContainer} />
 
+        <Router>
+          <div>
+            <header> <nav id="components-btn">
+                <button className="btn" ><Link className="components-btn" to="/">Home</Link></button>
+                <button className="btn" ><Link className="components-btn" to="/dog-list/">Dog List</Link></button>
+                <button className="btn" ><Link className="components-btn" to="/game1/">Game 1</Link></button>
+                <button className="btn" ><Link className="components-btn" to="/game2/">Game 2</Link></button>
+            </nav>
+            </header>
+           
+
+            <Route exact path="/" component={Home} />
+            <Route path="/dog-list" component={DogsListContainer} />
+            <Route path="/dog-breeds/:breed" component={DogBreedsImagesContainer} />
+            <Route path="/game1" component={Game1} />
+            <Route path="/game2" component={Game2} />
+
+          </div>
+        </Router>
       </header>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  
+  render(){
+    
+    return <div>
+      <h3 className="rr">Are you ready for some fun with dogs? </h3>
+      <img src="https://media2.giphy.com/media/xT0xeuOy2Fcl9vDGiA/giphy.gif" width="150px" alt=""/>
+    </div>
+  }
+  
+}
+
+export default (Home)
