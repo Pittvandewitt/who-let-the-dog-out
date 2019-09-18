db8f2a13a6c5decf237ea7382b01ce22502feb7d
pittvandewitt <thomasdewitt@outlook.com>
2019-09-18 14:37:26 +0200

    Reinitialize state on page refresh

--- src/actions/SetDogObjects.js
+++ src/actions/SetDogObjects.js
@@ -1,6 +1,33 @@
-export default function SetDogsObjects(dogData) {
-  return {
+import * as request from 'superagent'
+import store from '../store'
+
+function SetDogsObjects(dogData) {
+  return store.dispatch({
     type: 'SET_DOG_OBJECTS',
     payload: dogData
+  })
+}
+
+/* 
+ * Function that initializes the global state if it's emtpy
+ */
+export const getDogs = () => {
+  return (dispatch, getState) => {
+    const dogsList = getState()
+    if (dogsList.length !== 0) return
+
+    request('https://dog.ceo/api/breeds/list/all')
+      .then(data => {
+        const promises = Object.keys(data.body.message).map(dog => {
+          return request(`https://dog.ceo/api/breed/${encodeURIComponent(dog)}/images`)
+            .then(data => ({ breed: dog, images: data.body.message }))
+        })
+
+        Promise.all(promises).then(responses => SetDogsObjects(responses))
+      })
+      .catch(error => {
+        window.alert('An error occured while fetching data. Try reloading the page')
+        console.log(error)
+      })
   }
 }
---
+++ src/components/DogbreedsImages.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+export default function DogBreedsImages(props) {
+  
+  function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+  const shuffledArray = shuffle(props.images)
+  
+  const images = props.images !== null
+    ? shuffledArray.slice(0, 10).map(image => <img key={image} src={image} width="200px" alt="" />)
+    : 'Loading...'
+
+  return <div>
+    <h1>IMAGES DETAILS</h1>
+    <Link to="/">Go back to the index</Link>
+    {images}
+    {}
+  </div>
+}
