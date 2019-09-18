9c9e1c3843fdf21ab762a865f73b981b680ee3e3
Thomas W <thomasdewitt@outlook.com>
2019-09-18 12:56:14 +0000

    Cleanup App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import DogsListContainer from './components/DogsListContainer';
 import { Route } from 'react-router-dom'
 import DogBreedsImagesContainer from './components/DogBreedsImagesContainer';
 
-class App extends Component {
-
-  componentDidMount() {
-  }
-
-  render() {
+function App() {
     return <div className="App">
       <header className="App-header">
         <Route exact path="/" component={DogsListContainer} />
         <Route path="/dog-breeds/:breed" component={DogBreedsImagesContainer} />
       </header>
     </div >
-  }
 }
 
 export default App
