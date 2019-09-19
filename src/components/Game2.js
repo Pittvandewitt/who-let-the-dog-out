6aac2abbadfb053dffd11305d2d9c26c0bad607c
pittvandewitt <thomasdewitt@outlook.com>
2019-09-19 12:27:21 +0200

    Share common functions

--- src/components/Game2.js
+++ src/components/Game2.js
@@ -1,52 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { getDogs } from '../actions/SetDogObjects'
-import shuffle from '../actions/Shuffle'
+import { getRandomDogObject } from '../actions/gameFunctions'
+import { shuffle } from '../actions/gameFunctions'
 
 class Game2 extends Component {
   state = {
-    score: 0
+    score: 0,
+    level: 2
   }
 
   componentDidMount() {
     this.props.getDogs()
   }
 
-  getImage(collection) {
-    const dog = collection[this.getIntInRange(collection)]
-    const correctAnswer = dog.breed
-    const wrongAnswer = collection[this.getIntInRange(collection)].breed
-    const wrongAnswer1 = collection[this.getIntInRange(collection)].breed
-    const image = dog.images[this.getIntInRange(dog.images)]
-    return { correctAnswer, wrongAnswer, wrongAnswer1, image }
-  }
-
   getIntInRange = (range) => Math.floor(Math.random() * range.length - 1)
 
   verifyAnswer = (data) => (event) => {
     const clickedButtonName = event.target.firstChild.data
-    window.alert("That's " + (clickedButtonName === data.correctAnswer))
-    const increase = clickedButtonName === data.correctAnswer ? 1 : 0
-    this.setState({ score: this.state.score + increase })
+    const correctAnswer = clickedButtonName === data[0].breed
+    window.alert("That's " + correctAnswer)
+    const scoreIncrease = correctAnswer ? 1 : 0
+    const levelIncrease = this.state.score % 5 === 0 ? 1 : 0 // Edit difficulty here
+    this.setState({ score: this.state.score + scoreIncrease, level: this.state.level + levelIncrease })
   }
 
   render() {
     if (!this.props.dogs.length > 0) return <div>Loading...</div>
 
-    const data = this.getImage(this.props.dogs)
-    const buttons = [
-      <button onClick={this.verifyAnswer(data)}>{data.correctAnswer}</button>,
-      <button onClick={this.verifyAnswer(data)}>{data.wrongAnswer}</button>,
-      <button onClick={this.verifyAnswer(data)}>{data.wrongAnswer1}</button>
-    ]
+    const data = getRandomDogObject(this.props.dogs, this.state.level)
+    const buttons = []
+    for (let i = 0; i < this.state.level; i++)
+      buttons.push(<button onClick={this.verifyAnswer(data)}>{data[i].breed}</button>)
 
     return <div>
       <h1>What breed is this?</h1>
-      <div>
-        <div><img src={data.image} alt='dog'></img></div>
-        {shuffle(buttons).map(element => element)}
-        <div><text>Score: {this.state.score}</text></div>
-      </div>
+      <div><img src={data[0].images[this.getIntInRange(data[0].images)]} alt='dog'></img></div>
+      {shuffle(buttons).map(element => element)}
+      <div><text>Score: {this.state.score}</text></div>
     </div>
   }
 }
