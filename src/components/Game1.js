43ae983bcec9b8d16cf17078158c8c308afeefb3
Rezan92 <rezanibrahim8@gmail.com>
2019-09-20 14:25:05 +0200

    Hint for game one

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -8,24 +8,53 @@ class Game1 extends Component {
   state = {
     score: 0,
     wrong: 0,
-    gameOver: false
+    gameOver: false,
+    showHint: false
   }
   componentDidMount() {
     this.props.getDogs()
   }
+  
   handleClick = (event) => {
     if (event.target.name === 'correct') {
       alert('Good your answer is correct')
-      this.setState({ score: this.state.score + 1 })
+     this.setState({
+        score: this.state.score + 1
+      })
+     const hideImageHint=document.querySelector('.show')
+     hideImageHint.className="hide"
+      this.state.score === 4?alert('Good job you have 5 correct answers'): this.setState({
+        score: this.state.score + 1
+      })
+
     } else {
       alert(' Sorry your answer is wrong')
       this.setState({ wrong: this.state.wrong + 1 })
+      const hideImageHint=document.querySelector('.show')
+      hideImageHint.className="hide"
       if (this.state.wrong === 2) {
         alert('You got 3 answers wrong')
         this.setState({ gameOver: true })
+       
       }
     }
   }
+
+  getHint=(event)=>{
+    // if(!this.state.showHint){
+    //   this.setState({showHint:true})
+    // }else{
+    //   this.setState({showHint:false})
+    // }
+
+    if(event.target.querySelector('img').className==="hide"){
+       event.target.querySelector('img').className="show"
+    }else{
+  event.target.querySelector('img').className="hide"
+}
+  }
+ 
+
   tryAgain = () => {
     this.setState({ gameOver: false })
     this.setState({ score: 0 })
@@ -49,26 +78,27 @@ class Game1 extends Component {
     const correctImage = correctDog.images !== undefined
       ? correctDog.images[this.randomNumber(correctDog.images)] : "loading..."
     const Images = breeds.map(dog => dog.images[this.randomNumber(dog.images)])
-    console.log('the dogbreedimg', correctImage)
-    console.log('the dogbreed', correctbreed)
 
     const mix = [
       <button key={1}><img onClick={this.handleClick} name={'wrong'} src={Images[this.randomNumber(Images)]} width="200px" alt="" /></button>,
       <button key={2} ><img onClick={this.handleClick} name={"wrong"} src={Images[this.randomNumber(Images)]} width="200px" alt="" /></button>,
-      <button key={3}><img onClick={this.handleClick} name={'correct'} src={correctImage} width="200px" alt="" /></button>]
+      <button key={3} ><img onClick={this.handleClick} name={'correct'} src={correctImage} width="200px" alt="" /></button>]
+    
     return <div>
+      
       <h1>Choose the correct image</h1>
       <h3 className="question">Which image is {correctbreed} breed?</h3>
       {shuffle(mix)}
       <h3 >Your Score is : {this.state.score}</h3>
       <h3 >Your wrong answers : {this.state.wrong}</h3>
-
+      <button onClick={this.getHint}><img  className="hide" src={correctImage} width="200px" alt="" />Hint</button>
     </div>
   }
 
-  render() {
+  render() { 
     return <div>{!this.state.gameOver ? this.game() : this.gameOver()
-    }</div>
+    }
+    </div>
   }
 }
 const mapStateToProps = (state) => {
